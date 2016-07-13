6a92243be501c6482246ad348082b582cc8f5b97
jay nichols <jay_nichols_3@hotmail.com>
2016-07-13 15:26:14 -0500

    added css

---
+++ public/server-name-generator.php
@@ -0,0 +1,34 @@
+<?php
+
+$adjectiveArray = ['Clumsy', 'Sickly', 'Drunk', 'Orange', 'Tiresome', 'Annoying', 'Amateur', 'Exciting', 'Spicy', 'Taciturn'];
+$nounArray = ['Horse', 'Reebok', 'Florida', 'Brick', 'Stationery', 'Volkswagon', 'Mattress', 'Guitar', 'Air-Conditioner', 'Kettle Bell'];
+
+function shuffleAdjective($adjective) {
+	shuffle($adjective);
+	return $adjective[0];
+}
+
+function shuffleNoun($noun) {
+	shuffle($noun);
+	return $noun[0];
+}
+
+$randomAdjective = shuffleAdjective($adjectiveArray);
+$randomNoun = shuffleNoun($nounArray);
+
+$serverName = "{$randomAdjective} {$randomNoun}";
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Server Name Generator</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="/css/server-name-generator.css">
+</head>
+<body>
+	<h1 id="header">Server Name Generator</h1>
+	<h1 id="serverName"><?php echo $serverName; ?></h1> 
+
+</body>
+</html>
