f98c5d305f88a45340ee652b012f3215093837ef
jay nichols <jay_nichols_3@hotmail.com>
2016-07-15 15:51:12 -0500

    added bootstrap styling

--- public/login.php
+++ public/login.php
@@ -1,7 +1,5 @@
 <?PHP
 
-var_dump($_POST);
-
 
 if(!empty($_POST)){
 	$userName = $_POST['userName'];
@@ -10,7 +8,7 @@ if(!empty($_POST)){
 		header('Location: authorized.php');
 		exit;
 	} else {
-		$message = "Not Valid.";
+		$message = "Not a Valid Password.";
 	}
 
 } else {
@@ -25,17 +23,55 @@ if(!empty($_POST)){
 <html>
 <head>
 	<title>Login</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="/css/login.css">
+		<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </head>
 <body>
-	 <form method="POST" action="login.php">
-        <label>User Name</label>
-        <input type="text" name="userName"><br>
-        <label>Password</label>
-        <input type="text" name="password"><br>
-        <input type="submit">
-    </form>
 
-  	<h1><?= $message; ?><h1>
+	<div class="login-page">
+		<div class="form">
+		<!-- 	<form class="register-form">
+				<input type="text" placeholder="name"/>
+				<input type="password" placeholder="password"/>
+				<input type="text" placeholder="email address"/>
+	  			<button>create</button>
+	  			<p class="message">Already registered? <a href="#">Sign In</a></p>
+			</form> -->
+			<form method="POST" action="login.php" class="login-form">
+	  			<input type="text" name="userName" placeholder="username"/>
+	  			<input type="password" name="password" placeholder="password"/>
+	 			<button>Login</button>
+	  			<p class="message">Not registered? <a href="#">Create an account</a></p>
+			</form>
+  		</div>
+	</div>
+	 <!-- <form method="POST" action="login.php">
+		<label for="username">User Name</label>
+		<input type="text" id="username" name="userName" placeholder="Enter your User Name"><br>
+		<label for="password">Password</label>
+		<input type="password" id="password" name="password" placeholder="Enter your Password"><br>
+		<button type="submit">Login</button>
+	</form> -->
 
+	<h1><?= $message; ?><h1>
+
+ <script>
+// "use strict";
+
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+// alert(<?= $message; ?>);
+
+</script>
 </body>
 </html>
