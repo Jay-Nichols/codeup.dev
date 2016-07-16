67219cf17373b6778d6e90644d507e6e544d9ea8
jay nichols <jay_nichols_3@hotmail.com>
2016-07-16 18:18:52 -0500

    ping/pong game using GET requests

---
+++ public/ping.php
@@ -0,0 +1,29 @@
+<?php
+
+function pageController() {
+	if (isset($_GET['count'])) {
+		$count = $_GET['count'];
+	} else {
+		$count = 0;
+	}
+	return ['count' => $count];
+}
+
+extract(pageController());
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>PING</h1>
+	<h2><a href="pong.php?count=<?= $count +1; ?>">HIT</a></h2>
+	<h2><a href="ping.php">MISS</a></h2>
+	<h2>COUNTER: <?= $count; ?></h2>
+
+</body>
+</html>
