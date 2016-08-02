37045f5390a2628a6d5c9b2e814e91f85534c343
jay nichols <jay_nichols_3@hotmail.com>
2016-08-02 14:06:50 -0500

    Executing queries with MYSQL and PHP

---
+++ php/park_migration.php
@@ -0,0 +1,19 @@
+<?php
+
+require "db_connect.php";
+
+
+$query = 'DROP TABLE IF EXISTS national_parks';
+
+$dbc->exec($query);
+
+$parksTable = 'CREATE TABLE national_parks (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(240) NOT NULL,
+    date_established DATE,
+    area_in_acres DECIMAL(10,2),
+    PRIMARY KEY (id)
+    )';
+
+$dbc->exec($parksTable);
