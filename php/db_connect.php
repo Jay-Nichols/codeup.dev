a73a564a462a87586857e0172bdb6ae5ad09e81b
jay nichols <jay_nichols_3@hotmail.com>
2016-08-02 10:46:44 -0500

    Connecting to MySQL via PDO

---
+++ php/db_connect.php
@@ -0,0 +1,19 @@
+<?php
+
+define('DB_HOST', '127.0.0.1');
+
+define('DB_NAME', 'employees');
+
+define('DB_USER', 'vagrant');
+
+define('DB_PASS', 'vagrant');
+
+
+
+
+
+$dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME , DB_USER, DB_PASS);
+
+$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
