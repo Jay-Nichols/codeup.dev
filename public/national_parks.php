94e3b33c34eca9b37518fd0f269ead3d12222fcf
jay nichols <jay_nichols_3@hotmail.com>
2016-08-03 12:32:43 -0500

    added pagination

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,33 +1,48 @@
 <?php
-
-function pageController() {
-
+require_once "../php/db_connect.php";
 require_once "../php/park_migration.php";
 require_once "../php/park_seeder.php";
-// require "../src/Input.php";
+require_once "../src/Input.php";
 
-$stmt = $dbc->query('SELECT * FROM national_parks');
-// while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-//     print_r($row);
-// }
+function pageController($dbc) 
+{
 
 
-return ['parks' => $stmt->fetchAll(PDO::FETCH_ASSOC)];
+$page = !(Input::has('page')) ? 1 : max([Input::get('page', 1), 1]);
+$offset = $page * 4 - 4;
+$stmt = $dbc->query("SELECT * FROM national_parks ORDER BY name ASC LIMIT 4 offset {$offset} ");
+
+$parks = [];
+while ($park = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $parks[] = $park;
+}
+
+
+
+return ['parks' => $parks, 'page' => $page];
 
 
 }
 
-extract(pageController());
-var_dump($parks);
+extract(pageController($dbc));
+
 
 ?>
 
 <html>
 <head>
 	<title>National Parks</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="/css/adlister.welcome.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 </head>
 <body>
 	<h1>National Parks</h1>
+	
+	<form method= "POST">
 	<table>
 		<thead>
 			<tr>
@@ -50,6 +65,34 @@ var_dump($parks);
 		</tr>
 			<?php endforeach; ?>
 		</tbody>
-	</table>
+		<tfoot>
+             <tr>
+                 <td colspan="4">
+                     <!-- The values in this pagination control indicate you're currently viewing page 2 -->
+                     <nav aria-label="Page navigation" class="text-center">
+                         <ul class="pagination">
+                             <li>
+                                 <a href="?page=<?= $page - 1 ?>" aria-label="Previous">
+                                     <span aria-hidden="true">&laquo;</span>
+                                 </a>
+                             </li>
+                             <li><a href="?page=1">1</a></li>
+                             <li><a href="?page=2">2</a></li>
+                             <li><a href="?page=3">3</a></li>
+                             <li><a href="?page=4">4</a></li>
+                             <li><a href="?page=5">5</a></li>
+                             <li>
+                                 <a href="?page=<?= $page + 1 ?>" aria-label="Next">
+                                     <span aria-hidden="true">&raquo;</span>
+                                 </a>
+                             </li>
+                         </ul>
+                     </nav>
+                 </td>
+             </tr>
+          </tfoot>
+       </table>
+   </form>
+
 </body>
 </html>
