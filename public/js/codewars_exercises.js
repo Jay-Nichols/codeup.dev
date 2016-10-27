04f541af4cb5dfaf819c74404ece62ebd8dcac9b
jay nichols <jay_nichols_3@hotmail.com>
2016-10-27 14:17:40 -0500

    codewars javascript exercises

--- public/js/codewars_exercises.js
+++ public/js/codewars_exercises.js
@@ -365,8 +365,47 @@ getNumberFromString("this is number: 7");
 
 
 
+// Write a function, calculateTip(amount, rating) which calculates how much you need to tip based on the total amount of the bill and the service.
+
+// You need to consider the following ratings:
+
+// Terrible: tip 0%
+// Poor: tip 5%
+// Good: tip 10%
+// Great: tip 15%
+// Excellent: tip 20%
+// The rating is case insensitive. If an unrecognised rating is input, then you need to return:
+
+// "Rating not recognised" in Javascript, Python and Ruby...
+// ...or null in Java
+// Because you're a nice person, you always round up the tip, regardless of the service.
 
 
+function calculateTip(amount, rating) {
+	switch (rating) {
+		case 'Terrible':
+			console.log(Math.ceil(amount * 0));
+			break;
+		case 'Poor':
+			console.log(Math.ceil(amount * .05));
+			break;
+		case 'Good':
+			console.log(Math.ceil(amount * .10));
+			break;
+		case 'Great':
+			console.log(Math.ceil(amount * .15));
+			break;
+		case 'Excellent':
+			console.log(Math.ceil(amount * .20));
+			break;
+		default: 
+			console.log("Rating not recognized");
+
+	}
+}
+
+calculateTip(20, "Excellent");
+calculateTip(26.95, "good");
 
 
 
