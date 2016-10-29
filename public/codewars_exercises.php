76e24104148b15a8067822220e0201177a5490f2
jay nichols <jay_nichols_3@hotmail.com>
2016-10-29 14:25:50 -0500

    codewars php work

--- public/codewars_exercises.php
+++ public/codewars_exercises.php
@@ -15,7 +15,7 @@ class President
 
 	public function greet($name) 
 	{
-		echo "Hello INSERT_NAME_HERE, my name is $name, nice to meet you";  
+		echo "Hello $this->name, my name is $name, nice to meet you";  
 	}
 
 }
@@ -48,7 +48,7 @@ class Person
 
 	public function get_full_name()
 	{
-		echo "$first_name" . " " . "$last_name"; 
+		echo "$this->first_name" . " " . "$this->last_name"; 
 	}
 
 }
@@ -56,3 +56,68 @@ class Person
 $person1 = new Person("Jay", "Nichols");
 
 $person1->get_full_name();
+
+
+
+
+// Copy your working solution to the first Kata of this series and paste it here (you may want to complete that first if you haven't already done so).
+// Rename that class to CurrentUSPresident.
+// Convert all of the class properties into class constants and all of the class methods into static methods using the syntax taught in the lesson.
+// You can safely remove the code where an instance of the President class is created.
+
+class CurrentUSPresident
+{
+	const name = "Barack Obama";
+
+	public static function greet($name) 
+	{
+		echo "Hello " . self::name . ", my name is $name, nice to meet you";  
+	}
+
+}
+
+echo CurrentUSPresident::name;
+CurrentUSPresident::greet("Donald Trump");
+
+
+// Define a class called Person.
+// Since all Persons are of the species "Homo Sapiens", make that a class constant
+// Declare (but do not define yet) the 3 class properties $name, $age and $occupation. They should all be public.
+// Define a class constructor which accepts exactly three arguments in the following order: $name, $age, $occupation and store them in their respective properties.
+// Define a method called introduce which accepts no arguments and returns a string of the format "Hello, my name is NAME_HERE"
+// Define another method called describe_job which accepts no arguments and returns a string of the format "I am currently working as a(n) OCCUPATION_HERE" (NOTE: The "a(n)" part of the string is literal which means you do not have to create conditionals to check whether "a" or "an" should be used.)
+// When extraterrestrials arrive on Earth, all Persons are expected to greet them in exactly the same way. Therefore, define a static class method called greet_extraterrestrials which accepts an argument $species and returns a string of the format "Welcome to Planet Earth SPECIES_NAME_HERE!"
+
+Class Persons
+{
+	const species = "Homo Sapiens";
+
+	public $name;
+	public $age;
+	public $occupation;
+
+	public function __construct($name, $age, $occupation)
+	{
+		$this->name = $name;
+		$this->age = $age;
+		$this->occupation = $occupation;
+	}
+
+	public function introduce()
+	{
+		echo "Hello, my name is $name";
+	}
+
+	public function describe_job()
+	{
+		echo "I am currently working as a(n) $this->occupation";
+	}
+
+	public static function greet_extraterrestrials()
+	{
+		echo "Welcome to planet Earth" . " " . self::species;
+	}
+
+}
+$new_person = new Persons("Jay", "37", "future lottery winner");
+Persons::greet_extraterrestrials();
