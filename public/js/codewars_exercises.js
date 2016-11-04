cfec831407d511d44890f54ca4f332d1757314a7
jay nichols <jay_nichols_3@hotmail.com>
2016-11-04 13:11:08 -0500

    javascript codewars exercises

--- public/js/codewars_exercises.js
+++ public/js/codewars_exercises.js
@@ -408,4 +408,31 @@ calculateTip(20, "Excellent");
 calculateTip(26.95, "Good");
 
 
+// Rammstein needs a certain amount of fuel to create the fiery spectacle that is a Rammstein show. They need 100 hours for their new tour.
 
+// The input will be two variables: concentration of fuel (named concentration) and the number of barrels they have (named barrels). To get the number of hours of fuel Rammstein has available multiply the barrels by concentration.
+
+// If the number of hours of fuel available is less than 100, output should be a string "(number of hours of fuel needed to equal 100) Stunden mehr Benzin benötigt."
+// For example if you need five more hours the string should be "5 Stunden mehr Benzin benötigt." (this means 5 more hours of gasoline are needed in English)
+// If the number of hours of fuel is exactly 100, output should be a string that says "Perfekt!"
+// If the number of hours of fuel is more than 100, output should be the number of extra hours of fuel.
+// Help Rammstein so the show can go on!
+
+function feuerFrei(concentration, barrels) {
+	var hoursOfFuel = concentration * barrels;
+	var hoursOfFuelNeeded = 100 - hoursOfFuel;
+  if (hoursOfFuel < 100) {
+  	console.log(hoursOfFuelNeeded + " Stunden mehr Benzin benötigt.")
+  } else if (hoursOfFuel > 100) {
+  	console.log(hoursOfFuel);
+  } else {
+  	console.log("Perfekt");
+  }
+}
+
+
+
+feuerFrei(5,20);
+feuerFrei(5,200);
+feuerFrei(3,20);
+ 
