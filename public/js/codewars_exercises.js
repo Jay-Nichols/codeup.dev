120e1731c5aa306ef9273b90581df411be91bd90
jay nichols <jay_nichols_3@hotmail.com>
2016-11-28 13:46:43 -0600

    codewars javascript practice

--- public/js/codewars_exercises.js
+++ public/js/codewars_exercises.js
@@ -586,3 +586,82 @@ function DNAtoRNA(dna) {
 DNAtoRNA("TTTT");
 DNAtoRNA("GCAT");
 DNAtoRNA("GACCGCCGCC");
+
+
+
+
+// Given a random number:
+
+// C#: long;
+// C++: unsigned long;
+// You have to return the digits of this number within an array in reverse order.
+// Example:
+
+// 348597 => [7,9,5,8,4,3]
+
+console.log("-------------------");
+
+
+function digitize(n) {
+  
+  console.log(String(n).split('').map(Number).reverse());
+
+	
+}
+
+digitize(35231);
+
+
+console.log("-------------------------");
+
+
+// An Ironman Triathlon is one of a series of long-distance triathlon races organized by the World Triathlon Corporaion (WTC). It consists of a 2.4-mile swim, a 112-mile bicycle ride and a marathon (26.2-mile) (run, raced in that order and without a break. It hurts... trust me.
+
+// Your task is to take a distance that an athlete is through the race, and return one of the following:
+
+// If the distance is zero, return 'Starting Line... Good Luck!'
+
+// If the athlete will be swimming, return an object with 'Swim' as the key, and the remaining race distance as the value.
+
+// If the athlete will be riding their bike, return an object with 'Bike' as the key, and the remaining race distance as the value.
+
+// If the athlete will be running, and has more than 10 miles to go, return an object with 'Run' as the key, and the remaining race distance as the value.
+
+// If the athlete has 10 miles or less to go, return return an object with 'Run' as the key, and 'Nearly there!' as the value.
+
+// Finally, if the athlete has completed te distance, return "You're done! Stop running!"
+
+// All distance should be calculated to two decimal places.
+
+
+
+function iTri(s){
+	var total = 2.4 + 112 + 26.2;
+	if (s == 0) {
+		console.log('Starting Line... Good Luck!');
+	} else if (s <= 2.4) {
+		console.log("Swim: " + (total - s) + " to go!");
+	} else if (s > 2.4 && s < (2.4 + 112)) {
+		console.log("Bike: " + (total - s) + " to go!");
+	} else if (s > (total - 10)) {
+		console.log("Run: " + (total - s) + " to go!");
+	} else if (s <= (total - 10)) {
+		console.log("Run: nearly there.");
+	} else {
+		console.log("You're done! Stop running!");
+	}
+
+
+
+}
+
+
+
+iTri(36);
+iTri(103.5);
+iTri(2);
+
+
+
+
+
