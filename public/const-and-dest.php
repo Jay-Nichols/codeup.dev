03942d739dc5170e68709cc2e4b8e0b2c7a54673
jay nichols <jay_nichols_3@hotmail.com>
2017-04-20 12:20:03 -0500

    working with static methods and properties

--- public/const-and-dest.php
+++ public/const-and-dest.php
@@ -16,6 +16,14 @@ Class Bird
 		echo "{$this->genusName} {$this->speciesName} went extinct at " . time() . PHP_EOL;
 	}
 
+	public static $coolness = "All birds are cool";
+	public static $birdFact = "All birds have feathers";
+
+	public static function numberOfBirds()
+	{
+		return "There's billions of these guys and " . self::$birdFact;
+	}
+
 }
 
 $owl = new Bird("Bubo", "virginianus");
@@ -23,5 +31,9 @@ echo "The great horned owl's scientific name is: {$owl->genusName} {$owl->specie
 
 $cardinal = new Bird("Cardinalis", "cardinalis");
 echo "The cardinals scientific name is $cardinal->genusName $cardinal->speciesName" . PHP_EOL;
+
+echo "Here's a true statement about birds: " . Bird::$coolness . PHP_EOL;
+echo "Here's a fact about birds: " . Bird::$birdFact . PHP_EOL;
+echo "How many birds are there? " . Bird::numberOfBirds() . PHP_EOL;
 
 ?>
