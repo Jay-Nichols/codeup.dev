8460a71bef21fe8c35a837ff91cbdfd12e89cc27
jay nichols <jay_nichols_3@hotmail.com>
2017-04-21 10:53:21 -0500

    added overriding function

--- public/const-and-dest.php
+++ public/const-and-dest.php
@@ -20,22 +20,38 @@ Class Animal {
 
 Class Bird extends Animal
 {
+	public $commonName;
+
+	public function __construct($genusName, $speciesName, $commonName)
+	{
+		parent::__construct($genusName, $speciesName);
+		$this->commonName = $commonName;
+	}
+
+	public function birdName()
+	{
+		return $this->commonName;
+	}
+
+
 
 	public static $coolness = "All birds are cool";
 	public static $birdFact = "All birds have feathers";
 
+
+
 	public static function numberOfBirds()
 	{
-		return "There's billions of these guys and " . self::$birdFact;
+		return "There's billions of these guys and " . self::$birdFact . " and " . self::$coolness;
 	}
 
 }
 
-$owl = new Bird("Bubo", "virginianus");
-echo "The great horned owl's scientific name is: {$owl->genusName} {$owl->speciesName}" . PHP_EOL;
+$owl = new Bird("Bubo", "virginianus", "great horned owl");
+echo "The {$owl->birdName()}'s scientific name is: {$owl->genusName} {$owl->speciesName}" . PHP_EOL;
 
-$cardinal = new Bird("Cardinalis", "cardinalis");
-echo "The cardinals scientific name is $cardinal->genusName $cardinal->speciesName" . PHP_EOL;
+$cardinal = new Bird("Cardinalis", "cardinalis", "cardinal");
+echo "The {$cardinal->birdName()}'s scientific name is $cardinal->genusName $cardinal->speciesName" . PHP_EOL;
 
 echo "Here's a true statement about birds: " . Bird::$coolness . PHP_EOL;
 echo "Here's a fact about birds: " . Bird::$birdFact . PHP_EOL;
